340f09320002f7c7d49578446d5893d32067bbbf
Aaron Ackerman <theron17@gmail.com>
2013-11-29 20:42:38 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+var request = require('request');
+var nomnom  = require('nomnom');
+
+nomnom
+  .command('random')
+  .callback(function(opts) {
+    request
+      .get('http://lebowski.me/api/quotes/random')
+      .pipe(process.stdout);
+  })
+  .help("Get a random quote");
+
+nomnom
+  .command('quote')
+  .callback(function(args) {
+    var id = args[1];
+
+    if (!isNaN(id)) {
+      request
+        .get('http://lebowski.me/api/quotes/' + id)
+        .pipe(process.stdout);
+    }
+  })
+  .help("Get a quote by id");
+
+nomnom
+  .command('line')
+  .callback(function(args) {
+    var id = args[1];
+
+    if (!isNaN(id)) {
+      request
+        .get('http://lebowski.me/api/lines/' + id)
+        .pipe(process.stdout);
+    }
+  })
+  .help("Get a line by id");
+
+nomnom
+  .command('search')
+  .callback(function(args) {
+    var args = args['_'].slice(1);
+    var url = 'http://lebowski.me/api/quotes/search?term=' + encodeURI(args.join(' '));
+    console.log(url);
+    request
+      .get(url)
+      .pipe(process.stdout);
+  })
+  .help("Get a line by id");
+
+nomnom.parse();
